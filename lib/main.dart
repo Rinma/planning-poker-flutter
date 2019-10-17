598cf5e04cce0066cf13732620e2a56b23f8b6be
Marvin Dalheimer <me@marvin-dalheimer.de>
2019-10-17 07:24:03 +0200

    Add overview feature

--- lib/main.dart
+++ lib/main.dart
@@ -16,54 +16,127 @@ class PlanningPoker extends StatelessWidget {
           title: const Text("Planning Poker"),
         ),
         body: HomePage(),
+        floatingActionButton: OverviewFloatingButton(),
       ),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+final List<String> fibonacci = [
+  "0.5",
+  "1",
+  "2",
+  "3",
+  "5",
+  "8",
+  "13",
+  "20",
+  "40",
+  "100",
+  "?",
+  "@"
+];
+
+ScrollController controller;
+
+class HomePage extends StatefulWidget {
+  @override
+  HomeState createState() {
+    return HomeState();
+  }
+}
+
+class HomeState extends State<HomePage> {
+  @override
+  void initState() {
+    controller = ScrollController();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    const cardValues = [
-      "0.5",
-      "1",
-      "2",
-      "5",
-      "8",
-      "13",
-      "20",
-      "40",
-      "100",
-      "?",
-      "@"
-    ];
-    List<Widget> cards = [];
-
-    for (var value in cardValues) {
-      cards.add(Container(
-        width: MediaQuery.of(context).size.width,
-        alignment: Alignment.center,
-        color: Colors.transparent,
-        padding: EdgeInsets.all(15.0),
-        child: Container(
-          decoration: new BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(Radius.circular(40.0)),
-            boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 8)],
-          ),
-          child: Center(
-            child: Text(
-              value,
-              style: TextStyle(fontSize: 128),
+    return ListView.builder(
+      controller: controller,
+      scrollDirection: Axis.horizontal,
+      itemCount: fibonacci.length,
+      itemBuilder: (context, index) {
+        return Container(
+          key: Key("number-" + fibonacci[index]),
+          width: MediaQuery.of(context).size.width,
+          alignment: Alignment.center,
+          color: Colors.transparent,
+          padding: EdgeInsets.all(15.0),
+          child: Container(
+            decoration: new BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(40.0)),
+              boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 8)],
+            ),
+            child: Center(
+              child: Text(
+                fibonacci[index],
+                style: TextStyle(fontSize: 128),
+              ),
             ),
           ),
-        ),
-      ));
-    }
+        );
+      },
+    );
+  }
+}
 
-    return ListView(
-      scrollDirection: Axis.horizontal,
-      children: cards,
+class OverviewFloatingButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () => {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => Overview()),
+        )
+      },
+      child: Icon(Icons.dashboard),
+      backgroundColor: Colors.lightBlue,
+    );
+  }
+}
+
+class Overview extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Overview"),
+      ),
+      body: GridView.count(
+        crossAxisCount: 3,
+        children: List.generate(
+          fibonacci.length,
+          (index) {
+            return Container(
+              decoration: new BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 2)],
+              ),
+              margin: EdgeInsets.all(5),
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  controller.animateTo(
+                    index * MediaQuery.of(context).size.width,
+                    curve: Curves.linear,
+                    duration: Duration(milliseconds: 250),
+                  );
+                },
+                child: Center(
+                  child: Text(fibonacci[index], style: TextStyle(fontSize: 48)),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
